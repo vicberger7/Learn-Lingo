2acedfd1b628bc596de1f10c7a90644c1dd85905
vicberger7 <vicberger7@gmail.com>
2024-08-16 12:43:36 +0200

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,4 @@
-// import { initializeApp } from 'firebase/app';
-// import { getDatabase } from 'firebase/database';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAL1pMTCC2HkSK2hRRtNDD7a4tY4Z6AwgY',
-//   authDomain: 'learnlingo-155f6.firebaseapp.com',
-//   projectId: 'learnlingo-155f6',
-//   storageBucket: 'learnlingo-155f6.appspot.com',
-//   messagingSenderId: '605246707033',
-//   appId: '1:605246707033:web:3661b39f1d149d6233e848',
-//   databaseURL: 'https://learnlingo-155f6-default-rtdb.firebaseio.com',
-// };
-
-// export const app = initializeApp(firebaseConfig);
-// export const database = getDatabase(app);
 
 
 
